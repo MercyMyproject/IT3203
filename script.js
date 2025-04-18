4a5a76fdb804923c992064d03b2d7fe31109e67b
MercyMyproject <mnigatu1@students.kennesaw.edu>
2025-04-18 03:12:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 //Questions
 const questions = [
     {
-        question:"What is web application security?",
+    question: "What is web application security?",
     optionA: "is the parctice of protecting web applications from cyber threats",
     optionB: "building websites",
     optionC: "upgrading software,
         correctOption: "optionA"
 },
 {
-    question:"When is web application security needed?",
-         optionA: "Sometimes",
+    question: "When is web application security needed?",
+    optionA: "Sometimes",
     optionB: "Always",
     optionC: "Never",
         correctOption: "optionB"
 },
 {
-    question:"Why web application security is crucial"?
-     optionA: "Preventing attacks",
+    question: "Why web application security is crucial"?
+    optionA: "Preventing attacks",
     optionB: "Protecting sensetive data",
     optionC: "implementing website",
         correctOption: "optionA and B"
 },
 {
-    question:"How to practice web application security?",
+    question: "How to practice web application security?",
      optionA: "Enforce authorization",
     optionB: "Unsecure website",
     optionC: "Using unencrypted website",
         correctOption: "optionA"
 },
 {
-     question:"Fill in the blank, what is web application security requirements?",
+     question: "Fill in the blank, what is web application security requirements?",
  
-        correctAnswer: "Applications must validate all user input and block harmful data to prevent attacks like SQL injection and cross-site scripting(XSS)."
+    correctAnswer: "Applications must validate all user input and block harmful data to prevent attacks like SQL injection and cross-site scripting(XSS)."
 }
 ]
-//
+
+
 let shuffledQuestions = [] 
 function handleQuestions() {
-    while(shufffledQuestions.length <=9) {
+    while(shuffledQuestions.length <=9) {
         const random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)){
             shuffledQuestions.push(random)
@@ -45,9 +46,9 @@ function handleQuestions() {
     }
 }
 let questionNumber = 1 //holds question number
-let playerScore = 0 //holds the player score
-let wrongAttempt = 0 //
-let indexNumber = 0 //
+let playerScore = 5 //holds the player score
+let wrongAttempt = 2 //
+let indexNumber = 5 //
 
 //function for displaying next question
 function NextQuestion(index){
@@ -59,7 +60,7 @@ function NextQuestion(index){
     document.getElementById("option-one-label").InnerHTML = currentQuestion.optionA;
     document.getElementById("option-two-label").InnerHTML = currentQuestion.optionB;
     document.getElementById("option-three-label").InnerHTML = currentQuestion.optionC;
-    document.getElementById("option-one-label").InnerHTML = currentQuestion.correctAnswer;
+    document.getElementById("option-fillin-label").InnerHTML = currentQuestion.correctAnswer;
 
 }
 function checkForAnswer() {
@@ -77,7 +78,7 @@ function checkForAnswer() {
 
     //
     if (option.checked === true && option.value === currentQuestionAnswer) {
-        document.getElementById(correctOption).style.backgroundColor = "powderblue"
+        document.getElementById(correctOption).style.backgroundColor = "green"
         playerScore++ //
         indexNumber++ //
         //
@@ -85,23 +86,23 @@ function checkForAnswer() {
             questionNumber++
         }, 1000)
     }
-    //
+    
     else if (option.checked && option.value !== currentQuestionAnswer) {
         const wrongLabelId = option.labels[0].id
         document.getElementById(wrongLabelId).style.backgroundColor = "red"
         document.getElementById(correctOption).style.backgroundColor = "blue"
         wrongAttempt++
-        //
+        
         setTimeout(() => {
             questionNumber++
         }, 1000)
     }
-    // 
+    
     function handleNextquestion() {
         checkForAnswer()
         unCheckRadioButtons()
         setTimeout(() => {
-            if (indexNumber <=9) {
+            if (indexNumber <=1) {
                 NextQuestion(indexNumber)
             }
             resetOptionBackground()
@@ -131,7 +132,7 @@ function checkForAnswer() {
             remark = "Excellent!"
             remarkColor = "green"
         }
-        const playerGrade = (playerScore / 10) *100
+        const playerGrade = (playerScore / 5) *100
         //
         document.getElementById('remarks').innerHTML = remark
         document.getElementById('remarks').style.color = remarkColor
@@ -146,7 +147,7 @@ function checkForAnswer() {
         playerScore = 0
         wrongAttempt - 0
         indexNumber = 0
-        shuffledQuestions = []
+        shuffledQuestions = [5]
       NextQuestion(indexNumber)
         document.getElementById('score-modal').style.display = "none"
     }
